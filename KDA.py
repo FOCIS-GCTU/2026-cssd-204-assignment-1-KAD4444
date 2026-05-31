18ec23fc667edadfbaa134e6faf309152abc47cc
KAD4444 <kelvindanquah234@gmail.com>
2026-05-31 14:49:13 +0000

    assignment complete

---
+++ KDA.py
@@ -0,0 +1,36 @@
+# File: KDA.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: Kelvin Danquah Akurang
+# STUDENT ID: 2425403578
+# Email: 2425403578@live.gctu.edu.gh
+# Grader: Augustus Buckman
+#
+# On my honor, Kelvin Danquah AKurang, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
+def main():
+# Prints my initials in small and large stylized letters.
+
+    print()
+    print("...KDA")
+    print()
+
+main()
+
+print("...KKK......KKK.......DDDDDDDD...............AA.........")
+print("...KKK......KKK.......DDDDDDDDDD............AAAA........")
+print("...KKK....KKK.........DD.......DDD.........AA.AAA.......")
+print("...KKK..KK............DD........DD........AAA..AAA......")
+print("...KKKKKK.............DD........DD.......AAAA..AAAA.....")
+print("...KKKKKK.............DD........DD......AAAAAAAAAAAA....")
+print("...KKK..KKK...........DD........DD......AAAAAAAAAAAA....")
+print("...KKK....KKK.........DD.......DDD......AAA......AAA....")
+print("...KKK......KKK...**..DDDDDDDDDD....**..AAA......AAA..**")
+print("...KKK......KKK...**..DDDDDDDD......**..AAA......AAA..**")
+
+
+
+
+
